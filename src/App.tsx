7eb06afce5a5347ca3c938e5beff730235cee793
wicky4t <bhaiwicky5@gmail.com>
2025-10-16 21:33:21 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -226,7 +226,7 @@ function App() {
                 zIndex: img.zIndex,
                 animation: `slideUp 1s ease-out ${img.delay}s forwards`,
                 transform: 'translateY(100vh)',
-                position:  'absolute'              }}
+                      }}
             > 
               <img
                 src={img.src}
