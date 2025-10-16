82f53dab2703aded257114c0d8d185d8889bb1c3
wicky4t <bhaiwicky5@gmail.com>
2025-10-16 20:47:48 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -150,6 +150,19 @@ useEffect(() => {
       }
     });
 
+   // Portfolio section animation
+    if (portfolioSectionRef.current) {
+      gsap.to(portfolioSectionRef.current, {
+        y: -900,
+        scrollTrigger: {
+          trigger: portfolioSectionRef.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 1,
+        }
+      });
+    }
+    
     // Show/hide contact section
     ScrollTrigger.create({
       trigger: portfolioSectionRef.current,
